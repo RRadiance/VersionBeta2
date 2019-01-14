ddda784a776fcf3eb631dd02071b9e0771871ef6
Matthew Huynh <mattrhuynh@gmail.com>
2019-01-14 13:40:10 -0500

    Add ability to connect to pre-existing database

--- src/main/java/DatabaseInterface/SQLiteConnector.java
+++ src/main/java/DatabaseInterface/SQLiteConnector.java
@@ -18,11 +18,41 @@ public class SQLiteConnector {
 	
 	/**
      * Connect to the specified database. If the database does not exist,
-     * creates the database. Returns true if the database already existed,
-     * else false.
+     * creates the database. 
      * 
      * @param fileName the database file name
-     * @return a boolean if the 
+     * 
+     */
+    public static void connect(String fileName) {
+        Connection conn = null;
+        try {
+        	// We use a relative file path
+            String url = "jdbc:sqlite:databases/" + fileName;
+            
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+            
+            System.out.println("Connection to SQLite has been established.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+    
+    
+	/**
+     * Create a database with the specified name as fileName.
+     * Return true if the creation is successful, else false.
+     * 
+     * @param fileName the database file name
+     * @return boolean
      */
     public static Boolean createNewDatabase(String fileName) {
  
@@ -30,15 +60,13 @@ public class SQLiteConnector {
         String url = "jdbc:sqlite:databases/" + fileName;
  
         try (Connection conn = DriverManager.getConnection(url)) {
-        	// If the database does not exist
             if (conn != null) {
                 DatabaseMetaData meta = conn.getMetaData();
                 System.out.println("The driver name is " + meta.getDriverName());
                 System.out.println("A new database has been created.");
-                return false;
+                return true;
             }
-            return true;
- 
+            return false;
         } catch (SQLException e) {
             System.out.println(e.getMessage());
             return false;
@@ -51,6 +79,7 @@ public class SQLiteConnector {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        createNewDatabase("data.db");
+        // createNewDatabase("data.db");c
+    	connect("data.db");
     }
 }
